aca77ea14329aaff33b4786d7e7121a9458f1d0d
fencerValery <val-klimovch@yandex.ru>
2019-06-18 17:00:11 +0300

    ff

--- app/src/main/java/com/example/testtask/MainActivity.java
+++ app/src/main/java/com/example/testtask/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button addLine,deleteLine;
+    private Button addLine,deleteLine,saveLine,saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
